5fa755a1eed4cc3df38db9c8f1839ec7a585a132
AnmolkPandey06 <anmolkp0612@gmail.com>
2024-03-28 12:42:53 +0530

    UI Making

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
 
 import './App.css'
-
+import Loginpage from './pages/Login/Loginpage'
+import SignUppage from './pages/SignUp/SignUppage'
+import Home from './pages/Home/Home'
 function App() {
   return (
     <>
-      <div className='text-blue-400'>Hello</div>
+      <div className='p-4 h-screen flex items-center justify-center'>
+          {/* <Loginpage/> */}
+          {/* <SignUppage/> */}
+          <Home/>
+
+      </div>
+    
     </>
   )
 }
---
+++ frontend/src/components/SideBar/Sidebar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import SearchInput from './SearchInput'
+SearchInput
+import Conversations from './Conversations'
+import LogoutButton from './Logout'
+const Sidebar = () => {
+  return (
+    <div>
+      <div className='border-slate-500 p-6 flex flex-col mb-0'>
+      <SearchInput/>
+        <div className='divider px-3'></div>
+        <Conversations/>
+        <LogoutButton/>
+
+      </div>
+       
+    </div>
+  )
+}
+
+export default Sidebar
