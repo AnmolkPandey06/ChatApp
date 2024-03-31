cba46f15133e5b10b48d55077ab410e57b881435
AnmolkPandey06 <anmolkp0612@gmail.com>
2024-04-01 01:54:43 +0530

    ek aur

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 import express from "express";
 import dotenv from 'dotenv';
-import authroutes from './backend/routes/authroutes.js'
-import { connectToDB } from "./backend/db/connnectMongoose.js";
-import messageroutes from './backend/routes/messageroues.js'
-import userroutes from './backend/routes/userroutes.js'
+import authroutes from './routes/authroutes.js'
+import { connectToDB } from "./db/connnectMongoose.js";
+import messageroutes from './routes/messageroues.js'
+import userroutes from './routes/userroutes.js'
 import cookieParser from "cookie-parser";
-import { app ,server} from "./backend/socket.io/socket.js";
+import { app ,server} from "./socket.io/socket.js";
 import cors from 'cors';
 import path from "path";
 // const app=express();
@@ -26,11 +26,11 @@ app.use("/api/messages",messageroutes);
 app.use("/api/user",userroutes);
 
 
-app.use(express.static(path.join(__dirname,"/frontend/dist")))
+// app.use(express.static(path.join(__dirname,"/frontend/dist")))
 
-app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
-})
+// app.get("*",(req,res)=>{
+//     res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+// })
 
 const PORT=process.env.PORT||5000;
 server.listen(PORT,()=>{
